0921544cbf18ea41ecc25ea860b02acff79bcc76
Calin Cristian <calin.crist@gmail.com>
2019-10-05 16:38:37 +0300

    Init project with typescript.

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { FunctionComponent, useState } from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar
+} from 'react-native'
+
+const App: FunctionComponent<{}> = (props: {}) => {
+  return (
+    <View>
+      <Text>Texty</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default App
